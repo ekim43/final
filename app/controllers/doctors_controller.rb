d762762ac2e76298478e64cb716e89dd6748ac26
ekim43 <ekim2015@kellogg.northwestern.edu>
2015-05-06 14:43:10 -0500

    7 golden actions

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -8,4 +8,29 @@ class DoctorsController < ApplicationController
 	@doctor = Doctor.find_by(id: params["id"])
   end
 
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+	Doctor.create(params["doctor"])
+  	redirect_to doctors_url
+  end
+
+  def edit
+  	@doctor = Doctor.find_by(id: params["id"])
+  end
+
+  def update
+  	doctor = Doctor.find_by(id: params["id"])
+  	doctor.update(params["doctor"])
+  	redirect_to doctors_url
+  end
+
+ def destroy
+ 	doctor = Doctor.find_by(id: params["id"])
+ 	doctor.delete
+ 	redirect_to doctors_url
+ end
+
 end
